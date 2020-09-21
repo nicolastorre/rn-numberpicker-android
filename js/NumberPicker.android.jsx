fa7369fba498ab8797d595367da7a3c0dc7a8ed7
ntorre <n.torre@e2time.com>
2020-09-21 15:11:36 +0200

    Correction PropTypes

--- js/NumberPicker.android.jsx
+++ js/NumberPicker.android.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {requireNativeComponent} from 'react-native';
+import {requireNativeComponent, ViewPropTypes} from 'react-native';
 import PropTypes from 'prop-types';
 
 NumberPicker.propTypes = {
   items: PropTypes.array,
-  selectedItem: PropTypes.number,
+  selectedItem: PropTypes.string,
   onChange: PropTypes.func,
   textColor: PropTypes.string,
   fontSize: PropTypes.number,
-  style: PropTypes.Object
+  style: ViewPropTypes.style
 };
 
 NumberPicker.defaultProps = {
