4d43f0bebc3442f094906b0b3871840102044a97
ntorre <merlier220@gmail.com>
2020-12-01 16:47:38 +0100

    Correction github workflow test detox android

--- example/e2e/run_e2e_tests.sh
+++ example/e2e/run_e2e_tests.sh
@@ -6,8 +6,6 @@ ls . -all
 npm i -g envinfo detox-cli && envinfo
 npm install
 
-echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-mkdir -p /root/.android && touch /root/.android/repositories.cfg
 echo yes | $ANDROID_HOME/tools/bin/sdkmanager --channel=0 --verbose "system-images;android-28;default;x86_64"
 echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "Pixel_API_28" --package "system-images;android-28;default;x86_64" --sdcard 200M --device 11
 adb start-server
