ca6fec55958ba300bbe063fe85b17b50a4b5816c
ntorre <merlier220@gmail.com>
2020-12-01 16:59:27 +0100

    Correction github workflow test detox android

--- example/e2e/run_e2e_tests.sh
+++ example/e2e/run_e2e_tests.sh
@@ -10,6 +10,8 @@ echo yes | $ANDROID_HOME/tools/bin/sdkmanager --channel=0 --verbose "system-imag
 echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "Pixel_API_28" --package "system-images;android-28;default;x86_64" --sdcard 200M --device 11
 adb start-server
 
+$ANDROID_HOME/tools/bin/avdmanager list avd
+
 npx jetify
 npx detox build -c android.emu.release
 npx detox test -c android.emu.release --artifacts-location="e2e/__screenshots__" --take-screenshots="all" --headless
